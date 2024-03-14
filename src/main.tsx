0c8838d84f59e48cf1e5791547fba5b989a271a8
Darshan-V <mr.darshanv@gmail.com>
2024-03-14 16:29:05 +0530

    react entry file

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import * as ReactDOM from "react-dom/client";
+import * as ReactRouter from "react-router-dom";
+import React from "react";
+import LandingPage from "./pages/landingPage";
+import "./main.css";
+
+const router = ReactRouter.createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <>
+    <React.StrictMode>
+      <ReactRouter.RouterProvider router={router} />
+    </React.StrictMode>
+  </>
+);
