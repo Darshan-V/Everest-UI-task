83aab88d26533bf54851248f841a7331b6322b1a
Darshan-V <mr.darshanv@gmail.com>
2024-03-14 20:26:15 +0530

    add Hero component skeleton for the mobile page view

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import styles from "./index.module.css";
 import Layout from "../components/Layout";
+import Hero from "./components/Hero";
 
 const LandingPage: React.FC = () => {
   return (
     <Layout>
       <div className={styles.landingPage}>
-        <h1>Welcome to the Landing Page!</h1>
-        <p>
-          This is a responsive landing page component for web, mobile, and
-          tablet views.
-        </p>
+        <Hero />
       </div>
     </Layout>
   );
