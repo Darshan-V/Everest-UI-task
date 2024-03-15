1053f05a491d73bbe6b723725ca2d31948c1aa78
Darshan-V <mr.darshanv@gmail.com>
2024-03-15 18:12:46 +0530

    add MobileView menu modal component

---
+++ src/pages/components/MobileMenu.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import MobileMenuStyles from "./mobileMenu.module.css";
+
+import HamburgerCloseIcon from "../../assets/ui/hamburger-close.svg";
+import TopbarMobile from "../../assets/ui/topbar-mobile.png";
+
+type MobileMenuProps = {
+  closeMenu: (value: boolean) => void;
+};
+
+const MobileMenuList = ["this month", "skin", "hair", "bath", "sale", "log in"];
+const MobileMenu: React.FC<MobileMenuProps> = ({ closeMenu }) => {
+  return (
+    <div className={MobileMenuStyles.container}>
+      <img src={TopbarMobile} className={MobileMenuStyles.topbar} />
+      <div className={MobileMenuStyles.menuContainer}>
+        <img
+          src={HamburgerCloseIcon}
+          className={MobileMenuStyles.logo}
+          onClick={() => closeMenu(false)}
+        />
+        <div className={MobileMenuStyles.menuButtonsContainer}>
+          {MobileMenuList.map((item, index) => (
+            <a href="#" key={index}>
+              {item}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileMenu;
