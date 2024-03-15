3411df39c59efa6dd6cda8b74a1f84c8772f2a9c
Darshan-V <mr.darshanv@gmail.com>
2024-03-15 13:54:14 +0530

    update Footer component in the screen layout

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 
 import styles from "./layout.module.css";
+import Footer from "./Footer";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -13,7 +14,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <div className={styles.topbar} />
       <Header />
       <main>{children}</main>
-      <footer>{/* Footer content goes here */}</footer>
+      <Footer />
     </div>
   );
 };
